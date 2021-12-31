b870ca493b5dc60bfb297ceece3f8f68d92f2daa
Brian Tran <ninjapika500@outlook.com>
2021-12-31 11:18:57 -0800

    Created a general menu
    The menu is currently meant to display menu options and eventually run the respective programs

--- main.java
+++ main.java
@@ -1,9 +1,46 @@
 /**
  * main
  */
+import java.util.Scanner;
+
 public class main {
 
     public static void main(String[] args) {
-        System.out.println("Hello");
+        bool check  = True;
+        Scanner Input = new Scanner(System.in);
+        int userInput = 10;
+        while(check){
+            System.out.println("Welcome to the Weapon Generator/nChoose an option:/n 1. Create new weapon"+
+                "/n 2. Check preset weapons /n 3. Check preset enchantments /n 4. Check weapon properties"+
+                "/n 5. Check list of stored weapons /n 0. Exit program");
+            userInput = Input.nextInt();
+            switch (userInput) {
+                case 1:
+                    System.out.println("This option is currently in progress. Come back another time");
+                    break;
+                case 2:
+                    System.out.println("This option is currently in progress. Come back another time");
+                    break;
+                case 3:
+                    System.out.println("This option is currently in progress. Come back another time");
+                    break;
+                case 4:
+                    System.out.println("This option is currently in progress. Come back another time");
+                    break;
+                case 5:
+                    System.out.println("This option is currently in progress. Come back another time");
+                    break;
+                case 0:
+                    System.out.println("Thank you for using this Weapon generator/n/n/t Hope to see you again!");
+                    check = false;
+                    break;
+                default:
+                    System.out.println("Invalid Input/n/n/tRedisplaying menu");
+                    break;
+            }
+            
+
+        }
+    
     }
 }
