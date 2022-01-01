543c376f1a7333fb8ce7db2f8c70eb497e0b8031
Brian Tran <ninjapika500@outlook.com>
2021-12-31 18:18:33 -0800

    Closed the scanner object and fixed display of menu

--- main.java
+++ main.java
@@ -1,3 +1,4 @@
+
 /**
  * main
  */
@@ -6,13 +7,18 @@ import java.util.Scanner;
 public class main {
 
     public static void main(String[] args) {
-        boolean check  = true;
+        boolean check = true;
         Scanner Input = new Scanner(System.in);
         int userInput = 10;
-        while(check){
-            System.out.println("Welcome to the Weapon Generator/nChoose an option:/n 1. Create new weapon"+
-                "/n 2. Check preset weapons /n 3. Check preset enchantments /n 4. Check weapon properties"+
-                "/n 5. Check list of stored weapons /n 0. Exit program");
+        while (check) {
+            System.out.println("Welcome to the Weapon Generator \n" +
+                    "Choose an option:\n" +
+                    "\t 1. Create new weapon\n" +
+                    "\t 2. Check preset weapons\n" +
+                    "\t 3. Check preset enchantments\n" +
+                    "\t 4. Check weapon properties\n" +
+                    "\t 5. Check list of stored weapons\n" +
+                    "\t 0. Exit program\n");
             userInput = Input.nextInt();
             switch (userInput) {
                 case 1:
@@ -31,16 +37,18 @@ public class main {
                     System.out.println("This option is currently in progress. Come back another time");
                     break;
                 case 0:
-                    System.out.println("Thank you for using this Weapon generator/n/n/t Hope to see you again!");
+                    System.out.println("Thank you for using this Weapon generator\n\n\t Hope to see you again!");
                     check = false;
                     break;
                 default:
-                    System.out.println("Invalid Input/n/n/tRedisplaying menu");
+                    System.out.println(
+                            "Invalid Input\n..............................\n\tRedisplaying menu\n..............................\n");
                     break;
+
             }
-            
 
         }
-    
+        Input.close();
+
     }
 }
